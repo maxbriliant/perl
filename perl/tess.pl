f71153444b275bac60269c4f296e4dce38ad27c2
Maks Briliant <47421740+mxup@users.noreply.github.com>
2020-05-26 18:43:23 +0300

    Update tess.pl

--- perl/tess.pl
+++ perl/tess.pl
@@ -1,8 +1,10 @@
 #/usr/bin/perl
+
+#Tool for Extracting Text from Images
 use 5.1.0;
 use Image::OCR::Tesseract 'get_ocr';
  
-my $image = '/home/jack/Desktop/screenshot1.png';
+my $image = '/home/mx/Desktop/screenshot1.png';
 my $text = get_ocr($image);
 
 print $text;
